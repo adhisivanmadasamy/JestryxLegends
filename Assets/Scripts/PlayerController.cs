5601f25f06355107eb30a2823f55d0205078e9c6
Adhisivan Madasamy <adhi2000uk@gmail.com>
2024-03-22 17:09:11 +0000

    Char Anim HalfDone
    Character animation states are halfway done for Connor.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,7 +11,7 @@ public class PlayerController : MonoBehaviour
     float verticalLookRotation;
     bool grounded;
     Vector3 smoothMoveVelocity;
-    Vector3 moveAmount;
+    public Vector3 moveAmount;
 
     Rigidbody rb;
     PhotonView PV;
@@ -48,7 +48,7 @@ public class PlayerController : MonoBehaviour
         transform.Rotate(Vector3.up * Input.GetAxisRaw("Mouse X") * mouseSensitivity);
 
         verticalLookRotation += Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
-        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -70f, 70f);
 
         cameraHolder.transform.localEulerAngles = Vector3.left * verticalLookRotation;
 
@@ -58,6 +58,7 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
         moveAmount = Vector3.SmoothDamp(moveAmount, moveDir * (Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed), ref smoothMoveVelocity, smoothTime);
+        
     }
 
     void Jump()
